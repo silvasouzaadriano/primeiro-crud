a308ed3750da8fa383d4b9349e37537b270287a4
adriano.souza <adriano.souza@zpesystems.com>
2023-01-16 20:27:23 -0300

    created created user endpoint

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const port = 5000;
 
 app.use(express.json());
 
+const users = []
+
+app.get('/users', (request, response) => {
+  response.json({'teste': 'Esse é um teste!'})
+})
+
+app.post('/users', (request, response) => {
+  const id = request.body.id;
+  const name = request.body.name;
+  const email = request.body.email;
+
+  const user = { id, name, email}
+
+  users.push(user)
+
+  response.json(user);
+})
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
